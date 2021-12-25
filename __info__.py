6f0958ae591ee3c365f8d23e37054c399b7c0ba5
German Espinosa <germanespinosa@gmail.com>
2021-12-25 17:05:10 -0600

    firs version

---
+++ __info__.py
@@ -0,0 +1,64 @@
+def __module_version__():
+	return 0, 0, 1 
+
+
+def __module_name__():
+	return 'tcp-messages' 
+
+
+def __author__():
+	return 'german espinosa' 
+
+
+def __author_email__():
+	return 'germanespinosa@gmail.com' 
+
+
+def __package_description__():
+	return 'create json services easily' 
+
+
+def __install_requires__():
+	return ['json-cpp'] 
+
+
+def __url__():
+	return '' 
+
+
+def __license__():
+	return 'MIT' 
+
+
+def __license_file__():
+	return '../resources/license.txt' 
+
+
+def __readme_file__():
+	return '../resources/readme.md' 
+
+
+def __package_name__():
+	return 'tcp_messages' 
+
+
+def __files__():
+	return ['message_client.py', 'message_connection.py', 'message_router.py', 'message_server.py', 'util.py'] 
+
+
+def __setup_py__():
+	return '../setup/setup.py' 
+
+
+def __setup_cfg__():
+	return '../setup/setup.cfg' 
+
+
+def __root_folder__():
+	return 'src' 
+
+
+def __description__():
+	return 'create json services easily' 
+
+
