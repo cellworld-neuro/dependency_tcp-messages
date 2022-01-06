305452f017eb86b33e09c03b9b56bce20aadd8e1
German Espinosa <germanespinosa@gmail.com>
2022-01-06 17:31:07 -0600

    readded tests

--- catchtests/message.cpp
+++ catchtests/message.cpp
@@ -1,6 +1,43 @@
-#include<catch.h>
-#include <tcp_messages>
+#include <catch.h>
+#include <thread>
+#include <chrono>
+#include <tcp_messages.h>
 
-TEST_CASE("basic message"){
+using namespace tcp_messages;
+using namespace std;
 
+TEST_CASE("get_response") {
+    cout << get_response("h1", true) << endl;
+    cout << get_response("h1", 1) << endl;
+    cout << get_response("h1", "HELLO!") << endl;
+    cout << get_response("h1", Message("hola","1")) << endl;
+    cout << get_response("h1", true) << endl;
+}
+
+
+struct Test_service : Message_service {
+    Routes(
+            Add_route_with_response("H1",h1, int);
+            Add_route("stop", stop);
+            )
+    int h1(int i) { return i+1; }
+};
+
+using namespace std::chrono_literals;
+using namespace std::this_thread;
+TEST_CASE("test_service") {
+    Message_server<Test_service> s;
+    cout << "starting server" << endl;
+    s.start(6500);
+    thread t([](){
+        cout << "message wait start" << endl;
+        sleep_for(5s);
+        cout << "message wait ends" << endl;
+        Message_client c;
+        c.connect("127.0.0.1", 6500);
+        c.send_message(Message("stop"));
+    });
+    s.join();
+    cout << "stopping server" << endl;
+    t.join();
 }
--- include/tcp_messages/message_router.h
+++ include/tcp_messages/message_router.h
@@ -15,4 +15,42 @@
     }                                                                     \
 }
 
+namespace tcp_messages {
+    template<class T>
+    Message get_response(const std::string &request_header, T response) {
+        if constexpr (std::is_same_v<T, Message>) {
+            return response;
+        }
+        Message response_message;
+        response_message.header = request_header + "_response";
+        if constexpr (std::is_base_of<json_cpp::Json_base, T>::value) {
+            response_message.body = response.to_json();
+        } else if constexpr (std::is_same_v<char *, T> || std::is_same_v<const char *, T>) {
+            response_message.body = response;
+        } else if constexpr (std::is_same_v<bool, T> || std::is_same_v<const bool, T>) {
+            if (response) {
+                response_message.body = "success";
+            } else {
+                response_message.body = "fail";
+            }
+        } else {
+            response_message.body = std::to_string(response);
+        }
+        return response_message;
+    }
+}
+
+#define Add_route_with_response(HEADER, DESTINATION, ...) {               \
+    if (std::regex_match (message.header,std::regex(HEADER))) {           \
+        routed = true;                                                    \
+        try {                                                             \
+                auto response = DESTINATION(__VA_OPT__(message.get_body<__VA_ARGS__>())); \
+                send_message(tcp_messages::get_response(message.header, response));         \
+        } catch (...) {                                                   \
+            failed_route(message);                                        \
+        }                                                                 \
+    }                                                                     \
+}
+
+
 #define Routes(ADD_ROUTES) bool route(const tcp_messages::Message &message) override { bool routed = false; ADD_ROUTES; return routed; }
