c612ef2d1d22aaddd9e0f0f1cd324d4473d11e85
German Espinosa <germanespinosa@gmail.com>
2022-01-07 13:13:16 -0600

    added custom message constructor

--- include/tcp_messages/message.h
+++ include/tcp_messages/message.h
@@ -4,8 +4,12 @@
 namespace tcp_messages {
     struct Message : json_cpp::Json_object {
         Message () = default;
-        Message (const std::string &header, const std::string &body = "");
-        Message (const std::string &header, const json_cpp::Json_base &body);
+        Message (const std::string &header);
+        template <class T>
+        Message (const std::string &header, const T &body){
+            this->header = header;
+            set_body(body);
+        }
         template <class T>
         T get_body() const{
             if constexpr (std::is_same_v<T, std::string>) {
@@ -16,7 +20,16 @@ namespace tcp_messages {
             body >> wx;
             return x;
         }
-        void set_body(const json_cpp::Json_base &content);
+        template <class T>
+        void set_body(const T &body){
+            if constexpr (std::is_same_v<T, std::string>) {
+                this->body = body;
+            } else if constexpr (std::is_same_v<T, char *>) {
+                this->body = std::string(body);
+            } else if constexpr (std::is_base_of<json_cpp::Json_base ,T>::value) {
+                this->body = body.to_json();
+            }
+        }
         Json_object_members({
             Add_member(header);
             Add_member(body);
--- src/message.cpp
+++ src/message.cpp
@@ -2,19 +2,9 @@
 
 namespace tcp_messages {
 
-    void Message::set_body(const json_cpp::Json_base &new_content) {
-        body << new_content;
-    }
-
-    Message::Message(const std::string &header, const json_cpp::Json_base &body) :
-    header(header),
-    body(body.to_json()){
+    Message::Message(const std::string &header) :
+    header(header){
 
     }
 
-    Message::Message(const std::string &header, const std::string &body) :
-    header(header),
-    body(body){
-
-    }
 }
